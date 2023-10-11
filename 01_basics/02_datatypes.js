02bcc727796c5c3aa5cbf60e18d276285434f646
Amit kumar <amitaec.786@gmail.com>
2023-10-11 07:56:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all JS as newer version
+//alert(3+3) //we are using nodeJS, not brawser
+
+console.log(3
+    +
+    3) // code readability should be high
+console.log("Ami AT Google")
+
+
+// number => 2 to power 53
+// bigint => for very large value 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine => 
+// symbol => unique
+
+
+console.log(typeof undefine) //undefine
+console.log(typeof null) // object
