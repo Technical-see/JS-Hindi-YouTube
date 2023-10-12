9127c01e626dc54385046029b81be8200e389513
Amit kumar <amitaec.786@gmail.com>
2023-10-12 07:19:45 +0000

    Conversions of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// true => 1, false => 0
+
+let isLoggedIn = "Amit"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
