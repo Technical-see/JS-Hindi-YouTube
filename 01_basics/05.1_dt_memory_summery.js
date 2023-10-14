cbc5e40e72e3b3d11889c251ae93d7802c3de1d2
Amit kumar <amitaec.786@gmail.com>
2023-10-14 07:13:43 +0000

    convesionOperation

---
+++ 01_basics/05.1_dt_memory_summery.js
@@ -0,0 +1,23 @@
+// Statck(Premitive dataypes)       Heap(Non-premitive datatypes)
+
+let myYoutubeName = "High-Tech Galaxy"
+let anotherName = myYoutubeName
+
+anotherName = "Latest-Movies"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+
+
+let userOne = {
+      email: "user@google.com",
+      Upi : "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "amit@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
---
+++ 01_basics/05_datatypes_summery.js
@@ -0,0 +1,36 @@
+// Premitive / call by value
+
+// 7 dataTypes: number, string, boolean, null, undefine, symbol, bigint
+
+const score = 123
+const  scoreValue = "123.3"
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+//  const bigNumber = 3456543576654356754n
+ 
+// console.table([score, scoreValue, isLoggedIn, outsideTemp,  userEmail, id, anotherId, bigNumber ])
+
+
+// refference /Non-premitive
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "doga", "nagraj"];
+
+let myobj = {
+      name : "Amit",
+      age : 26,
+}
+
+let myfunction = function(){
+      console.log("HelloWorld");
+}
+
+console.table(typeof myfunction );
